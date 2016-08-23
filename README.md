a36208d93fb5989386322f4b4f3a5e735b3fc12b
Branden Sandahl <branden157@gmail.com>
2016-08-23 11:46:09 -0400

    FaceBook log in integration

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ BitFountain Android Tutorial to work with Android
 ## Features
 * Parse
 * App class to handle on app start stuff
+* FaceBook log in integration
--- app/src/main/java/com/sixtel/bitdate/App.java
+++ app/src/main/java/com/sixtel/bitdate/App.java
@@ -3,6 +3,8 @@ package com.sixtel.bitdate;
 import android.app.Application;
 import android.util.Log;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.parse.Parse;
 
 /**
@@ -17,5 +19,8 @@ public class App extends Application {
         Log.d("App Class", "App Started");
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "yTygxd7jhXpNtTPWy2poMjJR8bBtrG34IyUQxozg", "t9XBrODeYwkoH1OA45tWByK52mgnsOLUfkdmGMcm");
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
     }
 }
